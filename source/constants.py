5046e411b0e0848bc2534c34913595600d5125f4
Denys Herasymuk <42843889+denysgerasymuk799@users.noreply.github.com>
2022-04-19 00:30:21 +0300

    Added test model transform

--- source/constants.py
+++ source/constants.py
@@ -2,3 +2,5 @@ FORECAST_PERIOD = 24
 
 FEATURES_LAGS = [24 * i for i in range(1, 6)]
 RADIATION_LAGS = [24 * i for i in range(1, 6)]
+RECENT_RADIATION_LAGS = [i for i in range(1, 6)]
+MAX_DF_SHIFT = max(FEATURES_LAGS + RADIATION_LAGS)
