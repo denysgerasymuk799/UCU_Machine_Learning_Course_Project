184fe7cc37796313ade0b6f540c659eb432e565e
Denys Herasymuk <42843889+denysgerasymuk799@users.noreply.github.com>
2022-04-26 23:51:50 +0300

    Added tuned parameters for each ML and DL models

--- source/constants.py
+++ source/constants.py
@@ -1,7 +1,7 @@
-FORECAST_PERIOD = 24 * 3
+FORECAST_PERIOD = 24 * 3 # 3 days
 DECOMPOSED_SHIFT = FORECAST_PERIOD // 2 + (FORECAST_PERIOD // 2) % 24  # round to the closest number of days
 
-FEATURES_LAGS = [24 * i for i in range(1, 6)]
-RADIATION_LAGS = [24 * i for i in range(1, 6)]
+FEATURES_LAGS = [24 * 2 + 24 * i for i in range(1, 4)]
+RADIATION_LAGS = [24 * 2 + 24 * i for i in range(1, 4)]
 RECENT_RADIATION_LAGS = [i for i in range(1, 6)]
 MAX_DF_SHIFT = max(FEATURES_LAGS + RADIATION_LAGS)
