fb00ba2df01bdf88192b932c2a248f5b1a2ddfdf
Denys Herasymuk <42843889+denysgerasymuk799@users.noreply.github.com>
2022-04-26 20:50:35 +0300

    Added new version of 03 notebook

--- source/utils.py
+++ source/utils.py
@@ -1,4 +1,20 @@
+import logging
 import matplotlib.pyplot as plt
+
+
+class LogHandler(logging.StreamHandler):
+    def __init__(self):
+        logging.StreamHandler.__init__(self)
+        fmt = '\U00002705 [%(levelname)s] - [%(asctime)s] - (%(filename)s).%(funcName)s(%(lineno)d): %(message)s'
+        fmt_date = '%Y-%m-%d %H:%M:%S'
+        formatter = logging.Formatter(fmt, fmt_date)
+        self.setFormatter(formatter)
+
+
+logger = logging.getLogger('root')
+logger.setLevel('INFO')
+logging.disable(logging.DEBUG)
+logger.addHandler(LogHandler())
 
 
 def run_sequence_plot(df, y_column, title, from_datetime=None, to_datetime=None):
